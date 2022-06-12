dffb06fbd78c95c5331e218763cb5054903607fe
Abraham Azael <aamol.riv@gmail.com>
2022-06-11 21:35:21 -0500

    Configuracion de ConnectionString

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,16 +1,30 @@
+using BusinessLogic.Data;
 using BusinessLogic.Logic;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Configuration
+ConfigurationManager Configuration = builder.Configuration; //
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<MarketDbContext>(opt =>
+{
+    opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+})
 builder.Services.AddTransient<IProductoRepository, ProductoRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
 
 app.UseAuthorization();
 
